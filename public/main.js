22c854e1af748ca20809f35846c29d0930a58314
Lorelai Vennard <lorelai.vennard@skybettingandgaming.com>
2021-02-11 13:09:48 +0000

    progress bar in makeuphtml

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 // Function to open burger menu
-// Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon 
+// Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon
 function openMenu() {
   console.log("test");
   var x = document.getElementById("myTopnav");
@@ -10,34 +10,36 @@ function openMenu() {
   }
 }
 
-
-
 //Images on home page - function to move through images
 var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
--- public/survey.js
+++ public/survey.js
@@ -77,3 +77,24 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+/* Animated Progress Bar for results*/
+
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}

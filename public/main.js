a241a69c08004c985bc158cb5bb22997b80f011f
Julia.Afanasyeva <j.afanasjeva17@gmail.com>
2021-03-18 14:18:37 +0000

    good luck

--- public/main.js
+++ public/main.js
@@ -212,3 +212,19 @@ async function CreateTableFromJSONmakeUp() {
 function goBack() {
   window.history.back();
 }
+
+function getCookie(cname) {
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
